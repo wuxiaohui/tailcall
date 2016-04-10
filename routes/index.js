446b6827ac8dbaa4410fdd94c63b1e359d4121f3
xiaohui wu <xiaohui wu>
2016-04-11 00:20:11 +0800

    modified:   public/stylesheets/lib/material.css 	new file:   public/stylesheets/style.css 	modified:   routes/index.js 	modified:   views/index.ejs 	new file:   views/login.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '捕兽夹' });
+});
+router.get('/login', function (req, res, next) {
+    console.log('user');
+    res.render('login');
 });
 
 module.exports = router;
